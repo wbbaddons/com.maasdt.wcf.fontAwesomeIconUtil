785ae5705043f531f5dee773af8952d966abd6a6
Matthias Schmidt <gravatronics@live.com>
2014-01-29 16:18:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+Font Awesome Icon Util
+======================
+
+Font Awesome Icon Util provides PHP and JavaScript code for selecting Font Awesome icons.
+
+
+Code examples
+-------------
+
+PHP code for getting all available icons and checking if an icon is valid:
+
+```php
+<?php
+use wcf\util\FontAwesomeIconUtil;
+
+// fetch all icons which can be used as the options parameter for htmlOptions
+$icons = FontAwesomeIconUtil::getIcons()
+
+// check if a selected icon is valid
+$icon = 'html5';
+$isValidIcon = FontAwesomeIconUtil::isValid($icon);
+```
+
+HTML and JavaScript code for creating a dialog with an icon list:
+
+```html
+<span class="icon icon16 icon-th pointer jsFontAwesomeIconListButton jsTooltip" title="{lang}wcf.icon.fontAwesome.iconSelection{/lang}"></span>
+```
+
+```smarty
+// shows the icons in $icons as a dialog when clicking on an .jsFontAwesomeIconListButton element
+new WCF.Icon.FontAwesome.IconList([ {implode from=$icons item='__icon'}'{@$__icon}'{/implode} ], '.jsFontAwesomeIconListButton');
+```
+
+If you want to use the dialog icon list as an additional possibility to select icons, simply add the `data-select` attribute which contains the ID of `select` form element.
+
+```smarty
+{htmlOptions options=$icons name="icon" id="icon" selected=$icon}
+<span class="icon icon16 icon-th pointer jsFontAwesomeIconListButton jsTooltip" data-select="icon" title="{lang}wcf.icon.fontAwesome.iconSelection{/lang}"></span>
+```
+
+
+License
+-------
+
+This package licensed under the GNU Lesser General Public License which can be found in the LICENSE file in this folder.
---
+++ files/js/WCF.Icon.FontAwesome.js
@@ -0,0 +1,111 @@
+/**
+ * @author	Matthias Schmidt
+ * @copyright	2014 Maasdt
+ * @license	GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
+ * @package	com.maasdt.wcf.fontAwesomeIconUtil
+ */
+
+/**
+ * Initializes WCF.Icon namespace.
+ */
+if (!WCF.Icon) {
+	WCF.Icon = { };
+}
+
+/**
+ * Initializes WCF.Icon.FontAwesome namespace.
+ */
+WCF.Icon.FontAwesome = { };
+
+/**
+ * Handles icon list dialogs.
+ */
+WCF.Icon.FontAwesome.IconList = Class.extend({
+	/**
+	 * list of buttons triggering the icon lists
+	 * @var	array<jQuery>
+	 */
+	_buttons: [ ],
+	
+	/**
+	 * CSS selector for the buttons which trigger the icon list
+	 * @var	string
+	 */
+	_buttonSelector: '',
+	
+	/**
+	 * list of icon list dialogs
+	 * @var	object
+	 */
+	_iconLists: { },
+	
+	/**
+	 * names of the icons
+	 * @var	array<string>
+	 */
+	_icons: [ ],
+	
+	/**
+	 * Initializes a new WCF.Icon.FontAwesome.IconList list.
+	 * 
+	 * @param	array<string>	icons
+	 * @param	string		buttonSelector
+	 */
+	init: function(icons, buttonSelector) {
+		this._buttonSelector = buttonSelector;
+		this._icons = icons;
+		
+		this._buttons = $(this._buttonSelector);
+		if (!this._buttons.length) {
+			console.log('[WCF.Icon.FontAwesome.IconList] Selector does not match any element, aborting.');
+			return;
+		}
+		
+		this._buttons.click($.proxy(this._showIconList, this));
+		
+		this._iconListPrototype = $('<ul class="containerBoxList tripleColumned" />');
+		for (var $index in this._icons) {
+			var $iconName = this._icons[$index];
+			
+			$('<li><div class="box32"><span class="icon icon32 icon-' + $iconName + '" data-icon-name="' + $iconName + '" /> <p>' + $iconName + '</p></div></li>').appendTo(this._iconListPrototype);
+		}
+	},
+	
+	/**
+	 * Handles click on an icon to select it.
+	 * 
+	 * @param	Event		event
+	 */
+	_selectIcon: function(event) {
+		var $icon = $(event.currentTarget);
+		$('#' + $icon.data('select')).val($icon.data('iconName'));
+		
+		this._iconLists[$icon.parents('ul.containerBoxList.tripleColumned').data('buttonID')].wcfDialog('close');
+	},
+	
+	/**
+	 * Handles a click on a show icon list button.
+	 * 
+	 * @param	Event		event
+	 */
+	_showIconList: function(event) {
+		var $button = $(event.currentTarget);
+		var $buttonID = $button.wcfIdentify();
+		var $select = $button.data('select');
+		
+		if (this._iconLists[$buttonID] === undefined) {
+			this._iconLists[$buttonID] = $('<div />').hide().appendTo(document.body);
+			
+			var $ul = this._iconListPrototype.clone();
+			$ul.data('buttonID', $buttonID).appendTo(this._iconLists[$buttonID]);
+			
+			if ($select) {
+				$ul.find('span.icon.icon32').attr('title', WCF.Language.get('wcf.icon.fontAwesome.selectIcon')).addClass('pointer jsTooltip').data('select', $select).click($.proxy(this._selectIcon, this));
+			}
+		}
+		
+		this._iconLists[$buttonID].wcfDialog({
+			title: WCF.Language.get('wcf.icon.fontAwesome.dialogTitle')
+		});
+	}
+});
